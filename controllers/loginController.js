d4fc773c4508ac5aa31bd055b2c996aac227f91d
JohnFerrancol <88333166+JohnFerrancol@users.noreply.github.com>
2026-03-29 20:24:36 +0800

    fix: allow user input persist on authentication failure on login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,19 +1,29 @@
 import passport from '../auth/passport.js';
 
 const loginUsersGet = (req, res) => {
-  const authErrorMessages = req.flash('error');
-
+  console.log;
   res.render('login', {
     title: 'Log In',
-    messages: authErrorMessages,
-    formData: req.body || [],
+    authErrorMessages: req.flash('error'),
+    usernameInput: req.flash('usernameInput') || '',
   });
 };
 
-const loginUsersPost = passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash: true,
-});
+const loginUsersPost = (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) return next(err);
+
+    if (!user) {
+      req.flash('usernameInput', req.body.username);
+      req.flash('error', info.message);
+      return res.redirect('/login');
+    }
+
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+      return res.redirect('/');
+    });
+  })(req, res, next);
+};
 
 export { loginUsersGet, loginUsersPost };
