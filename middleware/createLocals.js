edfef5771d28dbe6771162168cb5fca2a5c7a0b2
JohnFerrancol <88333166+JohnFerrancol@users.noreply.github.com>
2026-03-29 20:38:02 +0800

    feat: dynamically render navbar when user is authenticated

--- middleware/createLocals.js
+++ middleware/createLocals.js
@@ -1,9 +1,19 @@
 const createLocals = (req, res, next) => {
-  res.locals.links = [
-    { href: '/', text: 'Odin Members' },
-    { href: '/register', text: 'Register' },
-    { href: '/login', text: 'Log In' },
-  ];
+  const baseLinks = [{ href: '/', text: 'Odin Members' }];
+
+  if (!req.user) {
+    res.locals.links = [
+      ...baseLinks,
+      { href: '/register', text: 'Register' },
+      { href: '/login', text: 'Log In' },
+    ];
+  } else {
+    res.locals.links = [
+      ...baseLinks,
+      { href: '/', text: req.user.username, isText: true },
+      { href: '/logout', text: 'Log Out' },
+    ];
+  }
 
   res.locals.errors = [];
   res.locals.formData = [];
